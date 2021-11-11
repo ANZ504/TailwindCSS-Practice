4bfb7f810d46973999600b7f52cf456f0bd6059a
Alejandro Adain Navarro Gorraiz <adain_navarro@hotmail.com>
2021-11-11 07:48:55 -0600

    Tailwind CSS

---
+++ Proyecto/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        rojito: '#ff5f40',
+        info: {
+          900: '#234e52',
+          800: '#285e61',
+        }
+      },
+    },
+    fontFamily: {
+      rale: ['Raleway'],
+    },
+  },
+  variants: {
+    borderWidth: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+}
